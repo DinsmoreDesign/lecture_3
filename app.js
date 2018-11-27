4a78cfcb7efcd1e68a2ceaeb774a92162a83b1ba
Derek Dinsmore <derek.dinsmore@switchthink.com>
2018-11-27 16:00:07 -0700

    Cleaning up some code.

--- app.js
+++ app.js
@@ -10,10 +10,19 @@ const server = http.createServer((req, res) => {
 
     if (url === '/') {
 
-        res.write('<html');
-        res.write('<head><title>Enter Message</title></head>');
-        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Submit</button></form></body>');
-        res.write('</html>');
+        res.write(`
+            <html>
+                <head>
+                    <title>Enter Message</title>
+                </head>
+                <body>
+                    <form action="/message" method="POST">
+                        <input type="text" name="message">
+                        <button type="submit">Submit</button>
+                    </form>
+                </body>
+            </html>
+        `);
 
         return res.end();
 
@@ -23,11 +32,7 @@ const server = http.createServer((req, res) => {
 
         const body = [];
 
-        req.on('data', (chunk) => {
-
-            body.push(chunk);
-
-        });
+        req.on('data', (chunk) => body.push(chunk));
 
         return req.on('end', () => {
 
@@ -56,10 +61,17 @@ const server = http.createServer((req, res) => {
 
     }
 
-    res.write('<html');
-    res.write('<head><title>My First Page</title></head>');
-    res.write('<body><h1>Hello from Node.js</h1></body>');
-    res.write('</html>');
+    res.write(`
+        <html>
+            <head>
+                <title>My First Page</title>
+            </head>
+            <body>
+                <h1>Hello from Node.js</h1>
+            </body>
+        </html>
+    `);
+
     res.end();
 
 });
